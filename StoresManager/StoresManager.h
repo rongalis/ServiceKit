7de1bb4b6f128398c434b987697b7d82b6ff510e
srikanth rongali <srikanth.rangoli@walgreens.com>
2017-10-27 16:44:08 -0500

    Initial set up

---
+++ StoresManager/StoresManager.h
@@ -0,0 +1,13 @@
+//
+//  StoresManager.h
+//  StoresManager
+//
+//  Created by srikanth rongali on 10/27/17.
+//  Copyright © 2017 srikanth rongali. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StoresManager : NSObject
+
+@end
---
+++ StoresManager/StoresManager.m
@@ -0,0 +1,13 @@
+//
+//  StoresManager.m
+//  StoresManager
+//
+//  Created by srikanth rongali on 10/27/17.
+//  Copyright © 2017 srikanth rongali. All rights reserved.
+//
+
+#import "StoresManager.h"
+
+@implementation StoresManager
+
+@end
